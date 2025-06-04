e01caf279aad940957683c71720d1efed7863b3f
Caio <caiolealvargas06@gmail.com>
2025-06-04 11:42:44 -0300

    back no front

---
+++ JS/exercicio.js
@@ -0,0 +1,11 @@
+﻿alert("JFono Beta 1.0")
+        const frm = document.querySelector("form");
+            const nome = document.getElementById("inNome");
+            console.log(`Olá ${nome} Seja bem vindo ao JFONO beta 1.0`)
+            
+            frm.addEventListener("submit",(e)=>{
+
+                console.log(nome);
+                e.preventDefault()
+
+            })
