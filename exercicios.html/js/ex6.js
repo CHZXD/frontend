ab7dd3418d33f850d07402882326500667fff433
Caio <caiolealvargas06@gmail.com>
2025-06-06 11:47:08 -0300

    JFONO

---
+++ exercicios.html/js/ex6.js
@@ -0,0 +1,19 @@
+﻿const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+const resp3 = document.querySelector("h5")
+
+frm.addEventListener("submit",(e)=>{
+    
+
+    const Veiculo = frm.inVeiculo.value
+    const entrada = Number (frm.inPreco.value)
+    const promocao = Math.floor(entrada / 2)
+    const divisao = promocao %12
+
+    resp1.innerText = Veiculo
+    resp2.innerText = `Entrada de R$: ${promocao}`
+    resp3.innerText = `12x de R$ ${divisao}`
+    
+    e.preventDefault() 
+})
